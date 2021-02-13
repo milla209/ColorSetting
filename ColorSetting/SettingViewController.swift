44896a1cf3b6b40563c921244c900dcece50a81d
Mila <milla209@mail.ru>
2021-02-13 15:26:00 +0300

    code 1

--- ColorSetting/SettingViewController.swift
+++ ColorSetting/SettingViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class SettingViewController: UIViewController {
     
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabelLeft: UILabel!
+    @IBOutlet var greenLabelLeft: UILabel!
+    @IBOutlet var blueLableLeft: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabelRight: UILabel!
+    @IBOutlet var greenLabelRight: UILabel!
+    @IBOutlet var blueLableRight: UIStackView!
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationItem.hidesBackButton = true
     }
@@ -16,10 +30,52 @@ class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        setColor()
+        setValue(for: redLabelLeft, greenLabelLeft, blueLableLeft)
     }
    
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender.tag {
+        case 0: redLabelLeft.text = string(from: sender)
+        case 1: greenLabelLeft.text = string(from: sender)
+        case 2: blueLableLeft.text = string(from: sender)
+        default: break
+        }
+    }
     
-
+    @IBAction func doneButtonPressed() {
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabelLeft.text = string(from: redSlider)
+            case 1: greenLabelLeft.text = string(from: greenSlider)
+            case 2: blueLableLeft.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
     /*
     // MARK: - Navigation
 
