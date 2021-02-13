922a1685c26da63786f894a1f01af165dcb440d6
Mila <milla209@mail.ru>
2021-02-13 13:54:20 +0300

    Setup VC

---
+++ ColorSetting/SettingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingViewController.swift
+//  ColorSetting
+//
+//  Created by Людмила Солопенко on 13.02.2021.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationItem.hidesBackButton = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+   
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
